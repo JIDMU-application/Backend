e7d27515af768a9d6c6bf7222b240684c6378e6b
markna551 <auttakrit.w@ku.th>
2022-05-14 14:58:00 +0700

    product review commit

---
+++ src/main/java/JIDMUM/review/controller/ReviewController.java
@@ -0,0 +1,44 @@
+package JIDMUM.review.controller;
+
+import JIDMUM.review.dto.ReviewRequest;
+import JIDMUM.review.dto.ReviewResponse;
+import JIDMUM.review.model.Review;
+import JIDMUM.review.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/review")
+public class ReviewController {
+
+    @Autowired
+    private ReviewService service;
+
+    @GetMapping
+    public List<Review> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public List<ReviewResponse> getAllReviewsForProduct(@PathVariable UUID id) {
+        return service.getAllReviewsForProduct(id);
+    }
+
+    @PostMapping
+    public ResponseEntity create(@Valid @RequestBody ReviewRequest review,
+                                 BindingResult result) {
+
+        if (result.hasErrors())
+            return new ResponseEntity<String>("Invalid request format", HttpStatus.UNPROCESSABLE_ENTITY);
+
+        service.addReview(review);
+        return new ResponseEntity<ReviewRequest>(review, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/JIDMUM/review/service/ReviewService.java
@@ -0,0 +1,45 @@
+package JIDMUM.review.service;
+
+import JIDMUM.review.dto.ReviewRequest;
+import JIDMUM.review.dto.ReviewResponse;
+import JIDMUM.review.model.Review;
+import JIDMUM.review.repository.ReviewRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository repository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<Review> getAll() {
+        return repository.findAll();
+    }
+
+    public List<ReviewResponse> getAllReviewsForProduct(UUID productId) {
+        List<Review> reviews = repository.findByProductId(productId);
+
+        List<ReviewResponse> dtos = reviews
+                .stream()
+                .map(review -> modelMapper.map(review, ReviewResponse.class))
+                .collect(Collectors.toList());
+
+        return dtos;
+    }
+
+    public void addReview(ReviewRequest reviewRequest) {
+        Review review = modelMapper.map(reviewRequest, Review.class);
+        review.setCreatedAt(Instant.now());
+        repository.save(review);
+    }
+}
