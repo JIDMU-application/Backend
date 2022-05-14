2f279bc859f57381cb3980bfeb0a83d826456ad9
Theetouch Kasemarnontana <58279552+lisbono2001@users.noreply.github.com>
2022-05-14 21:35:35 +0700

    Implement CORS to the API

--- src/main/java/JIDMUM/review/controller/ReviewController.java
+++ src/main/java/JIDMUM/review/controller/ReviewController.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/review")
 public class ReviewController {
